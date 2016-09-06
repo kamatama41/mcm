d855993d35f7d3f47d495c7bbe1f0bd9eaf01913
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-09-07 01:15:49 +0900

    Refactor startup/shutdown

--- lib/mcm/resource/automation_config.rb
+++ lib/mcm/resource/automation_config.rb
@@ -4,30 +4,40 @@ module MCM
       get :find, '/groups/:id/automationConfig'
       put :update, '/groups/:id/automationConfig'
 
-      def startup(id, process_name)
-        p = processes.detect{|p| p.name == process_name}
-        if p.disabled
-          p.disabled = false
+      def startup(id, process_names)
+        process_names = [process_names] unless process_names.is_a? Array
+        updated = false
+        processes.each do |p|
+          if process_names.include? p.name and p.disabled
+            p.disabled = false
+            updated = true
+          end
+        end
+        if updated
           self.class.update({id: id}.merge(self))
         end
       end
 
       def startup_all(id)
-        processes.each{|p| p.disabled = false}
-        self.class.update({id: id}.merge(self))
+        startup(id, processes.map(&:name))
       end
 
-      def shutdown(id, process_name)
-        p = processes.detect{|p| p.name == process_name}
-        if p.disabled
-          p.disabled =
+      def shutdown(id, process_names)
+        process_names = [process_names] unless process_names.is_a? Array
+        updated = false
+        processes.each do |p|
+          if process_names.include? p.name and !p.disabled
+            p.disabled = true
+            updated = true
+          end
+        end
+        if updated
           self.class.update({id: id}.merge(self))
         end
       end
 
       def shutdown_all(id)
-        processes.each{|p| p.disabled = true}
-        self.class.update({id: id}.merge(self))
+        shutdown(id, processes.map(&:name))
       end
     end
   end
